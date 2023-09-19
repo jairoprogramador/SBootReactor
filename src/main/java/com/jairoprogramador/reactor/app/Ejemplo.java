570ec8fdc5b6473ef9e36bee9aa3eaadc4639c44
Jairo Galvez <jairo.jggc@gmail.com>
2023-09-19 15:43:56 -0500

    flatMap

--- src/main/java/com/jairoprogramador/reactor/app/Ejemplo.java
+++ src/main/java/com/jairoprogramador/reactor/app/Ejemplo.java
@@ -4,6 +4,7 @@ import com.jairoprogramador.reactor.app.model.Usuario;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.Arrays;
 import java.util.List;
@@ -14,26 +15,15 @@ public class Ejemplo {
     public void execute(){
         List<String> nombresList = Arrays.asList("Andres", "Daniel", "Pedro", "Miguel", "Jose", "Daniel Juan", "Pedro Yonatan");
 
-        Flux<String> nombres = Flux.fromIterable(nombresList);
-        nombres.map(nombre -> new Usuario(nombre.toUpperCase(), null))
-                .filter(usuario -> usuario.getNombre().contains("DANIEL"))
-                .doOnNext( usuario -> {
-                    if(usuario == null || usuario.getNombre().isEmpty()){
-                        throw new RuntimeException("Usuario no puede ser null");
+        Flux.fromIterable(nombresList)
+                .map(nombre -> new Usuario(nombre.toUpperCase(), null))
+                .flatMap(usuario -> {
+                    if(usuario.getNombre().contains("DANIEL")){
+                        return Mono.just(usuario);
                     }
-                    System.out.println(usuario);
-                }).subscribe();
-
-        nombres.subscribe(
-                usuario -> log.info(usuario.toString()),
-                error -> {
-                    log.error(error.getMessage());
-                },
-                new Runnable() {
-                    @Override
-                    public void run() {
-                        log.info("Ha finalizado la ejecucion con exito");
-                    }
-                });
+                    return Mono.empty();
+                })
+                .subscribe(usuario -> log.info(usuario.toString())
+                );
     }
 }
