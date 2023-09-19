a85528b1381093e8aebe53048746157f48ad120b
Jairo Galvez <jairo.jggc@gmail.com>
2023-09-19 15:10:24 -0500

    inmutable

--- src/main/java/com/jairoprogramador/reactor/app/Ejemplo.java
+++ src/main/java/com/jairoprogramador/reactor/app/Ejemplo.java
@@ -5,24 +5,23 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Flux;
 
-import java.net.InetSocketAddress;
-
 public class Ejemplo {
 
     private static final Logger log = LoggerFactory.getLogger(Ejemplo.class);
     public void execute(){
-        Flux<Usuario> nombres = Flux.just("Andres", "Daniel", "Pedro", "Miguel", "Jose", "Daniel Juan", "Pedro Yonatan")
-                .map(nombre -> new Usuario(nombre.toUpperCase(), null))
+        Flux<String> nombres = Flux.just("Andres", "Daniel", "Pedro", "Miguel", "Jose", "Daniel Juan", "Pedro Yonatan");
+
+        nombres.map(nombre -> new Usuario(nombre.toUpperCase(), null))
                 .filter(usuario -> usuario.getNombre().contains("DANIEL"))
                 .doOnNext( usuario -> {
                     if(usuario == null || usuario.getNombre().isEmpty()){
                         throw new RuntimeException("Usuario no puede ser null");
                     }
                     System.out.println(usuario);
-                });
+                }).subscribe();
 
         nombres.subscribe(
-                usuario -> log.info(usuario.getNombre()),
+                usuario -> log.info(usuario.toString()),
                 error -> {
                     log.error(error.getMessage());
                 },
